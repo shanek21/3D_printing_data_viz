0e1f2d5a547598bb7db296d0272f6edb8c8b6ae1
mary-keenan <mary.keenan@students.olin.edu>
2017-02-22 21:36:53 -0500

    ADD: can get google sheets data, might need to redo authorization for other laptops, not sure

---
+++ polar_purpose.py
@@ -0,0 +1,93 @@
+#getting data -- google sheets API
+from __future__ import print_function
+import httplib2
+import os
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+#plotting data -- plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def get_data():
+    """Gets data from Google Sheet with print form responses and returns it as dictionary.
+    parameters: none
+    returns: list of lists --> list of rows of data
+    returns: dictionary of tuples --> dict[row_num] = ((purpose, print time))
+
+    Pulls data from print form: https://docs.google.com/spreadsheets/d/1_etu4C5YXYpfngERdZUV7wIIIKNwiiUOYfgId8Tb2iE/edit#gid=2089240762
+    """
+    
+    data_dict = dict() #create data dictionary we'll be returning
+    data_lists = []
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+
+    spreadsheetId = '1_etu4C5YXYpfngERdZUV7wIIIKNwiiUOYfgId8Tb2iE' #ACTUAL ID
+    #GET PURPOSE LIST
+    rangeName = 'Form Responses 1' #sheet_name!cell range
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', []) #store values in list
+   
+    # #GET
+    # rangeName = 'Form Responses 1!H2:H' #sheet_name ! cell range
+    # result = service.spreadsheets().values().get(
+    #     spreadsheetId=spreadsheetId, range=rangeName).execute()
+    # duration_values = result.get('values', []) #store values in list
+
+    if not values:
+        print('No data found.')
+    else:
+        for row in values:
+        	data_lists.append(row)
+
+
+if __name__ == '__main__':
+    get_data()
